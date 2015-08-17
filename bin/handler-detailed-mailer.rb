8e870a1f7ca1977663db1972906033f6ba35741e
Matt Jones <mattjones@yieldbot.com>
2015-08-17 10:16:09 -0400

    [sensu_v2]convert input to STDIN

--- bin/handler-detailed-mailer.rb
+++ bin/handler-detailed-mailer.rb
@@ -25,8 +25,8 @@ class DetailedMailer < Sensu::Handler
     settings['devops-mailer'][name]
   end
 
-  def short_name
-    @event['client']['name'] + '/' + @event['check']['name']
+  def short_name(check_ouput)
+    check_ouput['client']['name'] + '/' + check_ouput['check']['name']
   end
 
   def action_to_string
@@ -59,18 +59,18 @@ class DetailedMailer < Sensu::Handler
     end
   end
 
-  def template_vars
+  def template_vars(check_ouput)
     @config = {
-      monitored_instance    => @event['client']['name'],
-      incident_timestamp    => Time.at(@event['check']['issued']),
-      instance_address      => @event['client']['address'],
-      check_name            => @event['check']['name'],
-      check_command         => @event['check']['command'],
-      check_state           => define_status(@event['check']['status']),
-      num_occurrences       => @event['occurrences'],
+      monitored_instance    => check_ouput['client']['name'],
+      incident_timestamp    => Time.at(input['check']['issued']),
+      instance_address      => check_ouput['client']['address'],
+      check_name            => check_ouput['check']['name'],
+      check_command         => check_ouput['check']['command'],
+      check_state           => define_status(check_ouput['check']['status']),
+      num_occurrences       => check_ouput['occurrences'],
       notification_comment  => '#YELLOW', # the comment added to a check to silence it
       notification_author   => '#YELLOW', # the user that silenced the check
-      condition_duration    => "#{@event['check']['duration']}s",
+      condition_duration    => "#{input['check']['duration']}s",
       check_output          => '#YELLOW',
       sensu_env             => define_sensu_env,
       alert_type            => action_to_string,
@@ -85,6 +85,8 @@ class DetailedMailer < Sensu::Handler
   end
 
   def handle
+    check_output = JSON.parse(STDIN.read)
+
     mail_to                   = get_setting('mail_to')
     mail_from                 = get_setting('mail_from')
 
@@ -98,10 +100,10 @@ class DetailedMailer < Sensu::Handler
     smtp_authentication       = get_setting('smtp_authentication') || :plain
     smtp_enable_starttls_auto = get_setting('smtp_enable_starttls_auto') == 'false' ? false : true
 
-    subject = "#{define_sensu_env} #{action_to_string}  #{@event['check']['name']} on #{@event['client']['name']} is #{@event['check']['status']}"
+    subject = "#{define_sensu_env} #{action_to_string}  #{check_output['check']['name']} on #{check_output['client']['name']} is #{check_output['check']['status']}"
 
     # YELLOW
-    gem_base = `/opt/sensu/embedded/bin/gem environment gemdir`.gsub("\n",'')
+    gem_base = `/opt/sensu/embedded/bin/gem environment gemdir`.gsub("\n", '')
     template_path = "#{gem_base}/gems/dhoulmagus-#{Dhoulmagus::Version::STRING}/templates"
 
     Mail.defaults do
@@ -125,24 +127,8 @@ class DetailedMailer < Sensu::Handler
       delivery_method delivery_method.intern, delivery_options
     end
 
-    # mail = Mail.deliver do
-    #   to mail_to
-    #   from mail_from
-    #   subject subject
-    #
-    #   # text_part do
-    #   #   body 'This is plain text'
-    #   # end
-    #
-    #   html_part do
-    #     content_type 'text/html; charset=UTF-8'
-    #     template = 'templates/email.erb'
-    #     body = ERB.new(acquire_template(template)).result
-    #   end
-    # end
-
     begin
-      template_vars
+      template_vars(check_output)
       timeout 10 do
         Mail.deliver do
           to mail_to
@@ -156,7 +142,7 @@ class DetailedMailer < Sensu::Handler
         puts 'mail -- sent alert for ' + short_name + ' to ' + mail_to.to_s
       end
     rescue Timeout::Error
-      puts 'mail -- timed out while attempting to ' + @event['action'] + ' an incident -- ' + short_name
+      puts 'mail -- timed out while attempting to ' + check_output['action'] + ' an incident -- ' + short_name(check_output)
     end
   end
 end
