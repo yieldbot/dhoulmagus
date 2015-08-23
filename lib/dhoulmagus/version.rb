f5b320925e60b8a22af44b3e9e31cb7dc6e9a525
Matt Jones <mattjones@yieldbot.com>
2015-08-23 18:03:03 -0400

    [sensu_v2]auto-pull settings from config

--- lib/dhoulmagus/version.rb
+++ lib/dhoulmagus/version.rb
@@ -1,8 +1,11 @@
 require 'json'
 
 # encoding: utf-8
+# main entry point method
+# @since 0.0.1
 module Dhoulmagus
-  # This defines the version of the gem
+  # define the version of the gem
+  # @since 0.0.1
   module Version
     MAJOR = 0
     MINOR = 0
@@ -13,12 +16,20 @@ module Dhoulmagus
     NAME   = 'Dhoulmagus'
     BANNER = "#{NAME} v%s"
 
+    # pull the version into the gemspec for use with Jenkins
+    # @since 0.0.1
     module_function
 
+    # format the version
+    #
+    # @return [string] a properly formated version string
     def version
       format(BANNER, STRING)
     end
 
+    # format the version
+    #
+    # @return [string] a properly formated json version
     def json_version
       {
         'version' => STRING
